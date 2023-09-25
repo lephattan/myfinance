fe5c78c6f43e02a0244a6b702a226696405ff2a6
Tan Le <tanle2207@gmail.com>
2023-09-25 13:54:26 +0700

    feat: Start migrating main app from iris to fiber

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"myfinace/controller"
 	"myfinace/controller/htmx"
@@ -11,76 +10,53 @@ import (
 	"myfinace/helper"
 	"myfinace/service"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/accesslog"
 	"github.com/kataras/iris/v12/mvc"
-	"github.com/kataras/iris/v12/view"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	app_env := env.ReadEnv("APP_ENV", "production")
-	ac := makeAccessLog()
-	defer ac.Close()
+	// app_env := env.ReadEnv("APP_ENV", "production")
+	views := MakeViews()
 
-	app := iris.New()
-	app.Logger()
+	app := fiber.New(fiber.Config{Views: views})
+	MakeAccessLog(app)
 
-	app.UseRouter(ac.Handler)
-	app.Get("/ping", pong).Describe("health check")
-
-	app.RegisterView(makeView(app_env))
-
-	mvc.Configure(app.Party("greet"), setup)
-	mvc.Configure(app.Party("ticker"), tickerSetup)
-	mvc.Configure(app.Party("portfolio"), portfolioSetup)
-	mvc.Configure(app.Party("transaction"), transactionSetup)
-
-	// HTMX
-	mvc.Configure(app.Party("/htmx/components/transaction"), htmxComponentSetup)
-	mvc.Configure(app.Party("/htmx/components/ticker"), htmxTickerSetup)
-
-	app.Get("/", getRoot)
-	app.Listen("0.0.0.0:8080")
-}
-
-func makeView(e env.Env) *view.BlocksEngine {
-	view := iris.Blocks("./views", ".html")
-	if e == env.DEV {
-		view = view.Reload(true)
-	}
-	view.AddFunc("UnixTimeFmt", helper.UnixTimeFmt)
-	return view
-}
-
-func makeAccessLog() *accesslog.AccessLog {
-	ac := accesslog.File("./access.log")
-	ac.AddOutput(os.Stdout)
-	// The default configuration:
-	ac.Delim = '|'
-	ac.TimeFormat = "2006-01-02 15:04:05"
-	ac.Async = false
-	ac.IP = true
-	ac.BytesReceivedBody = true
-	ac.BytesSentBody = true
-	ac.BytesReceived = false
-	ac.BytesSent = false
-	ac.BodyMinify = true
-	ac.RequestBody = true
-	ac.ResponseBody = false
-	ac.KeepMultiLineError = true
-	ac.PanicLog = accesslog.LogHandler
-	// Default line format if formatter is missing:
-	// Time|Latency|Code|Method|Path|IP|Path Params Query Fields|Bytes Received|Bytes Sent|Request|Response|
+	log.Fatal(app.Listen("0.0.0.0:8080"))
+	// defer ac.Close()
 	//
-	// Set Custom Formatter:
-	ac.SetFormatter(&accesslog.JSON{
-		Indent:    "  ",
-		HumanTime: true,
-	})
-	// ac.SetFormatter(&accesslog.CSV{})
-	// ac.SetFormatter(&accesslog.Template{Text: "{{.Code}}"})
-	return ac
+	// app := iris.New()
+	// app.Logger()
+	//
+	// app.UseRouter(ac.Handler)
+	// app.Get("/ping", pong).Describe("health check")
+	//
+	// app.RegisterView(makeView(app_env))
+	//
+	// mvc.Configure(app.Party("greet"), setup)
+	// mvc.Configure(app.Party("ticker"), tickerSetup)
+	// mvc.Configure(app.Party("portfolio"), portfolioSetup)
+	// mvc.Configure(app.Party("transaction"), transactionSetup)
+	//
+	// // HTMX
+	// mvc.Configure(app.Party("/htmx/components/transaction"), htmxComponentSetup)
+	// mvc.Configure(app.Party("/htmx/components/ticker"), htmxTickerSetup)
+	//
+	// app.Get("/", getRoot)
+	// app.Listen("0.0.0.0:8080")
+}
+
+func MakeViews() *html.Engine {
+	engine := html.New("./views", ".html")
+	engine.AddFunc("UnixTimeFmt", helper.UnixTimeFmt)
+	return engine
+}
+
+func MakeAccessLog(app *fiber.App) {
+	app.Use(logger.New())
 }
 
 func getRoot(ctx iris.Context) {
