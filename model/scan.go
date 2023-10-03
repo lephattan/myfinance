e1800e7cf3501c50c566239b5c335e2a97756959
Tan Le <tanle2207@gmail.com>
2023-10-03 20:33:09 +0700

    feat: add func ModelScan for dynamic scan sql rows to stuct model

---
+++ model/scan.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"database/sql"
+	"reflect"
+)
+
+// Dynamic Scan rows to dest struct
+func ModelScan(dest interface{}, rows *sql.Rows) error {
+	v := reflect.ValueOf(dest).Elem()
+	pointers := []any{}
+	for i := 0; i < v.NumField(); i++ {
+		value := v.Field(i).Addr().Interface()
+		pointers = append(pointers, value)
+	}
+	return rows.Scan(pointers...)
+}
