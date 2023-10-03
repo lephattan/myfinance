0f0a06f6819192e5f9e1d5b98ed7aec2dea05fd6
Tan Le <tanle2207@gmail.com>
2023-10-03 14:20:49 +0700

    feat: [migration] add price to tickers table

---
+++ migrations/4_add_price_to_tickers.down.sql
@@ -0,0 +1,3 @@
+-- table tickers
+ALTER TABLE tickers DROP current_price ;
+ALTER TABLE tickers DROP price_updated_at ;
---
+++ migrations/4_add_price_to_tickers.up.sql
@@ -0,0 +1,3 @@
+-- table tickers
+ALTER TABLE tickers ADD current_price INTEGER;
+ALTER TABLE tickers ADD price_updated_at INTEGER;
