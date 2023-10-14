f35181f864315b292813cc62292a9fecb032a8ba
Tan Le <tanle2207@gmail.com>
2023-10-14 20:24:36 +0700

    fix: fix ticker price_updated_at trigger bug

--- migrations/6_create_ticker_price_updated_at_trigger.up.sql
+++ migrations/6_create_ticker_price_updated_at_trigger.up.sql
@@ -1,5 +1,5 @@
 Create Trigger update_ticker_price_updated_at Update Of current_price on tickers
 BEGIN 
-	UPDATE tickers SET price_updated_at = unixepoch('now');
+	UPDATE tickers SET price_updated_at = unixepoch('now') Where symbol = old.symbol And new.current_price != old.current_price;
 END
 
