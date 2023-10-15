8d8b0239e6e02dc76ba40d283a44c70c4293b988
Tan Le <tanle2207@gmail.com>
2023-10-15 15:35:13 +0700

    feat: move web-components to root

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,19 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import summary from 'rollup-plugin-summary';
-import {terser} from 'rollup-plugin-terser';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
+import summary from "rollup-plugin-summary";
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 
 const options = {
   onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
+    if (warning.code !== "THIS_IS_UNDEFINED") {
       console.error(`(!) ${warning.message}`);
     }
   },
   plugins: [
-    replace({'Reflect.decorate': 'undefined'}),
+    replace({ "Reflect.decorate": "undefined" }),
     resolve(),
     /**
      * This minification setup serves the static site generation.
@@ -40,11 +40,11 @@ function component(name) {
   return {
     input: `build/${name}.js`,
     output: {
-      file: `../assets/js/web-components/${name}.js`,
-      format: 'esm',
+      file: `./assets/js/web-components/${name}.js`,
+      format: "esm",
     },
     ...options,
   };
 }
 
-export default [component('pie-chart')];
+export default [component("pie-chart")];
